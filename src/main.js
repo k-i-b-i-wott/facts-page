0b402782afd447ca5705ef5a5a9ee79635a93e4e
Kibiwott <bk.kibiwott@gmail.com>
2025-03-02 15:55:11 +0300

    Complete code fetching cat facts

--- src/main.js
+++ src/main.js
@@ -2,17 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
     
     document.getElementById('factbtn').addEventListener('click', getCatFacts);
     document.getElementById('imgbtn').addEventListener('click', getCatImages);
+    document.getElementById('outputValue').innerHTML='';
+    document.getElementById('spinner').style.display='none';
  
 });
 const outputValue= document.getElementById('outputValue');
 const quote = document.getElementById('quote');
 const spinner = document.getElementById('spinner');
-
+const catImg = document.getElementById('catImg');
 
 
 async function getCatFacts() {
+    outputValue.innerHTML='';
+       
     spinner.style.display='block';
-    outputValue.innerHTML='';    
+       
     const userLimit = parseInt( quote.value) || 1;
     const limit = Math.min(userLimit, 50);
     try {
@@ -27,10 +31,11 @@ async function getCatFacts() {
         
         setTimeout(() => {
             outputValue.innerHTML=listHTML
+            outputValue.style.display="flex"; 
             spinner.style.display='none';
         },2000);
 
-        // outputValue.innerHTML=JSON.stringify(data, null, 2);
+        
     } catch (error) {
         
         console.log(error);
@@ -43,15 +48,27 @@ async function getCatFacts() {
 async function getCatImages() {
     spinner.style.display='block';
     outputValue.innerHTML='';    
-    const userLimit = parseInt( quote.value) || 1;
+    const userLimit = parseInt(catImg.value) || 1;
     const limit = Math.min(userLimit, 10);
     try {
-        const response = await fetch(`https://api.thecatapi.com/v1/images/search?count=${catImg.value}`);
+        const response = await fetch(`https://api.thecatapi.com/v1/images/search?count=${limit}`);
         const data = await response.json();
         console.log(data);
-        outputValue.innerHTML = data
+        let images = `<div style="display:flex; flex-wrap: wrap; justify-content:center; align-items: center; gap:2rem"></div>`;
+        data.forEach(cat => {
+            images +=`<img src="${cat.url}" alt="cat" width="50px" height="50px " object-fit="cover" >`
+        });
+        images += '</div>'
+
+        setTimeout(() => {    
+            outputValue.innerHTML=images    
+            outputValue.style.display="flex"; 
+            spinner.style.display='none';
+        },1000);    
     } catch (error) {
         console.log(error);
-    }
+        outputValue.innerHTML=`<p style="color:red;">Failed to load the image</p>`
+        spinner.style.display='none'; 
+       }
 }
-getCatImages();``
+
