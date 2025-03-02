83d65df1b37f99d5a9595343062e6ed5a5c0bedc
Kibiwott <bk.kibiwott@gmail.com>
2025-03-02 15:00:29 +0300

    Completed the cat Facts part and the spinner

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 const outputValue= document.getElementById('outputValue');
 const quote = document.getElementById('quote');
+const spinner = document.getElementById('spinner');
+
 
 
 async function getCatFacts() {
+    spinner.style.display='block';
+    outputValue.innerHTML='';    
     const userLimit = parseInt( quote.value) || 1;
     const limit = Math.min(userLimit, 50);
     try {
@@ -20,20 +24,29 @@ async function getCatFacts() {
             listHTML+=`<li>${element}</li>`
         })
         listHTML+=`</ol>`
-        outputValue.innerHTML=listHTML
-
-
+        
+        setTimeout(() => {
+            outputValue.innerHTML=listHTML
+            spinner.style.display='none';
+        },2000);
 
         // outputValue.innerHTML=JSON.stringify(data, null, 2);
     } catch (error) {
+        
         console.log(error);
+        spinner.style.display='none';
     }
+    
 }   
 
 
 async function getCatImages() {
+    spinner.style.display='block';
+    outputValue.innerHTML='';    
+    const userLimit = parseInt( quote.value) || 1;
+    const limit = Math.min(userLimit, 10);
     try {
-        const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=${catImg.value}`);
+        const response = await fetch(`https://api.thecatapi.com/v1/images/search?count=${catImg.value}`);
         const data = await response.json();
         console.log(data);
         outputValue.innerHTML = data
